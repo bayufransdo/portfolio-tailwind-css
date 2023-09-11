708a67cbcb87d2b530ef1ec8383d3b849315aad1
Bayu Maulana <bayufransdo@gmail.com>
2023-09-12 00:12:52 +0700

    update button klik langsung hide dan tombol back to top

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,12 +2,17 @@
 
 window.onscroll = function () {
     const header = document.querySelector("header");
+    const toTop = document.querySelector("#to-top");
     const fixedNav = header.offsetTop;
 
     if (window.scrollY > fixedNav) {
         header.classList.add("navbar-fixed");
+        toTop.classList.add("flex");
+        toTop.classList.remove("hidden");
     } else {
         header.classList.remove("navbar-fixed");
+        toTop.classList.add("hidden");
+        toTop.classList.remove("flex");
     }
 };
 
@@ -24,5 +29,20 @@ hamburger.addEventListener("click", function () {
     } else {
         navMenu.classList.add("scale-0");
         navMenu.classList.remove("scale-100");
+    }
+});
+
+// Klik di luar Hamburger
+window.addEventListener("click", function (e) {
+    if (e.target != navMenu && e.target != hamburger) {
+        if (navMenu.classList.contains("scale-0")) {
+            navMenu.classList.add("scale-100");
+            hamburger.classList.add("hamburger-active");
+            navMenu.classList.remove("scale-0");
+        } else {
+            navMenu.classList.add("scale-0");
+            hamburger.classList.remove("hamburger-active");
+            navMenu.classList.remove("scale-100");
+        }
     }
 });
